bdaf5ab0b1ad5029feaecd9318252a04d97dee2d
lesnyrumcajs <lesny.rumcajs@gmail.com>
2018-08-17 18:39:52 +0200

    removed descriptions

--- src/tst/set1/challenge4.cpp
+++ src/tst/set1/challenge4.cpp
@@ -3,13 +3,7 @@
 #include "../../xor_decryptor.hpp"
 
 /**
-Detect single-character XOR
-
-One of the 60-character strings in this file has been encrypted by single-character XOR.
-
-Find it.
-
-(Your code from #3 should help.)
+ * Detect single-character XOR
 */
 
 namespace {
